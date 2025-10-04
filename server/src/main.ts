95249dadd365a2780ca21c2263840a9c2ecdfa33
huuyii <3134846106@qq.com>
2025-10-04 11:13:05 +0800

    feat：使用http-proxy-middleware实现http模块转发

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,13 @@ async function bootstrap() {
     origin: 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
+  app.use('/api/test', createProxyMiddleware({
+    target: 'http://localhost:3000',
+    changeOrigin: true,
+    pathRewrite: {
+      '^/api/test': '/public'
+    }
+  }))
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
